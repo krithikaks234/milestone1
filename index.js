1e04803254288a6e69c99559328ab7ce948a4a56
krithikaks <134272947+krithikaks234@users.noreply.github.com>
2024-11-30 22:46:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-'use strict';
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-module.exports = require('./loose-envify')(process.env);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
